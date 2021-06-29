54bb6064d8f1801a29e41d87f6b5a0725f19e472
Eric Hicks <ehicks05@yahoo.com>
2021-06-29 18:35:33 -0400

    add comment minimizing and humanized time-ago

--- src/components/Comments.js
+++ src/components/Comments.js
@@ -1,4 +1,7 @@
 import React, { useState } from "react";
+import TimeAgo from "timeago-react";
+import { FiMinusSquare, FiPlusSquare } from "react-icons/all";
+
 import CommentForm from "./CommentForm.js";
 
 const deleteComment = async (commentId) => {
@@ -25,38 +28,56 @@ const Comments = ({ comments }) => {
 };
 
 const Comment = ({ comment }) => {
+  const [minimized, setMinimized] = useState(false);
   const [showReplyForm, setShowReplyForm] = useState(false);
+
   const indent = `ml-${comment.level}`;
   return (
     <div className={`${indent} m-2 p-2 border`} key={comment.id}>
-      <span>
-        <span>{comment.author}</span>
-        <span title={"id: " + comment.id}>{comment.content}</span>
-      </span>
-      <div className="flex pt-1 gap-4">
-        <button
-          className="p-0.5 border"
-          onClick={() => setShowReplyForm(!showReplyForm)}
-        >
-          Reply
+      <div className="flex gap-4">
+        <button onClick={() => setMinimized(!minimized)}>
+          {minimized ? <FiPlusSquare /> : <FiMinusSquare />}
         </button>
-        <button
-          className="p-0.5 border"
-          onClick={() => deleteComment(comment.id)}
-        >
-          Delete
-        </button>
-      </div>
-
-      {showReplyForm && (
-        <CommentForm
-          postId={comment.postId}
-          parentCommentId={comment.id}
-          toggleReplyBox={setShowReplyForm}
+        <span className="text-xs">{comment.author.username}</span>
+        <TimeAgo
+          title={comment.createdAt}
+          className="text-xs"
+          datetime={comment.createdAt}
         />
-      )}
+      </div>
+      {!minimized && (
+        <>
+          <div>
+            <span>{comment.content}</span>
+          </div>
+          <div className="flex pt-1 gap-4">
+            <button
+              className="p-0.5 border"
+              onClick={() => setShowReplyForm(!showReplyForm)}
+            >
+              Reply
+            </button>
+            <button
+              className="p-0.5 border"
+              onClick={() => deleteComment(comment.id)}
+            >
+              Delete
+            </button>
+          </div>
 
-      {comment.comments?.length > 0 && <Comments comments={comment.comments} />}
+          {showReplyForm && (
+            <CommentForm
+              postId={comment.postId}
+              parentCommentId={comment.id}
+              toggleReplyBox={setShowReplyForm}
+            />
+          )}
+
+          {comment.comments?.length > 0 && (
+            <Comments comments={comment.comments} />
+          )}
+        </>
+      )}
     </div>
   );
 };
