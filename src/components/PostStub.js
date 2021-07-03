25b7800a22fa2ae59f62327cc7a4f3fe92e0f84c
Eric Hicks <ehicks05@yahoo.com>
2021-07-03 11:58:38 -0400

    run prettier

--- src/components/PostStub.js
+++ src/components/PostStub.js
@@ -3,29 +3,31 @@ import { Link } from "react-router-dom";
 import TimeAgo from "timeago-react";
 
 const PostStub = ({ post, i }) => {
-    const bgClass =
-    i % 2 === 0
-      ? "bg-gray-50 dark:bg-gray-900"
-      : "bg-white dark:bg-black";
+  const bgClass =
+    i % 2 === 0 ? "bg-gray-50 dark:bg-gray-900" : "bg-white dark:bg-black";
   return (
     <div className={`p-2 border dark:border-gray-600 ${bgClass}`}>
-        <div className="flex gap-2">
-          {i !== undefined && <div className="">{`${i + 1}.`}</div>}
-          <div className="">
-            <div className="text-lg"><a href={post.link} target="_blank" rel="noopener noreferrer">{post.title}</a></div>
-            <div className="flex gap-4">
-              <span className="text-xs">{post.author.username}</span>
-              <TimeAgo
-                title={post.createdAt}
-                className="text-xs"
-                datetime={post.createdAt}
-              />
-            </div>
-      <Link to={`/posts/${post.id}`}>
-            <div className="text-sm">{`${post.commentCount} comments`}</div>
-      </Link>
+      <div className="flex gap-2">
+        {i !== undefined && <div className="">{`${i + 1}.`}</div>}
+        <div className="">
+          <div className="text-lg">
+            <a href={post.link} target="_blank" rel="noopener noreferrer">
+              {post.title}
+            </a>
           </div>
+          <div className="flex gap-4">
+            <span className="text-xs">{post.author.username}</span>
+            <TimeAgo
+              title={post.createdAt}
+              className="text-xs"
+              datetime={post.createdAt}
+            />
+          </div>
+          <Link to={`/posts/${post.id}`}>
+            <div className="text-sm">{`${post.commentCount} comments`}</div>
+          </Link>
         </div>
+      </div>
     </div>
   );
 };
