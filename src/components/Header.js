24cc85ebf458c0b4bda80ad9e5514b10eb153b8d
Eric Hicks <ehicks05@yahoo.com>
2021-07-06 00:05:39 -0400

    add fancy header from tailwind

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,18 +1,220 @@
-import React from "react";
-import { FaBars, FaTimes } from "react-icons/fa";
-import { Link } from "react-router-dom";
+import { Fragment } from "react";
+import { Disclosure, Menu, Transition } from "@headlessui/react";
+import { HiOutlineBell, HiOutlineMenu, HiOutlineX } from "react-icons/all";
+import { Link, NavLink, useLocation } from "react-router-dom";
+import { useAuth0 } from "@auth0/auth0-react";
+import md5 from "md5";
 
-const Header = ({ title, showSettings, setShowSettings }) => {
+const navigation = [
+  { name: "Hot", href: "/" },
+  { name: "Top", href: "/top" },
+  { name: "New", href: "/new" },
+];
+
+function classNames(...classes) {
+  return classes.filter(Boolean).join(" ");
+}
+
+export default function Header() {
+  const location = useLocation();
+  const { loginWithRedirect, logout, isAuthenticated, user } = useAuth0();
+
   return (
-    <header className="flex p-4 justify-between">
-      <Link to="/">
-        <img src="arclogo.png" alt="logo" className="h-16" />
-      </Link>
-      <div role="button" onClick={() => setShowSettings(!showSettings)}>
-        {showSettings ? <FaTimes /> : <FaBars />}
-      </div>
-    </header>
-  );
-};
+    <Disclosure as="nav" className="bg-gray-800">
+      {({ open }) => (
+        <>
+          <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
+            <div className="relative flex items-center justify-between h-16">
+              <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
+                {/* Mobile menu button*/}
+                <Disclosure.Button className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
+                  <span className="sr-only">Open main menu</span>
+                  {open ? (
+                    <HiOutlineX className="block h-6 w-6" aria-hidden="true" />
+                  ) : (
+                    <HiOutlineMenu
+                      className="block h-6 w-6"
+                      aria-hidden="true"
+                    />
+                  )}
+                </Disclosure.Button>
+              </div>
+              <div className="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
+                <div className="flex-shrink-0 flex items-center">
+                  <Link to={"/"}>
+                    <img
+                      className="block lg:hidden h-8 w-auto"
+                      src="/lightningbolt.svg"
+                      alt="Workflow"
+                    />
+                  </Link>
+                  <Link to={"/"}>
+                    <img
+                      className="hidden lg:inline h-8 w-auto"
+                      src="/lightningbolt.svg"
+                      alt="Workflow"
+                    />
+                    <img
+                      className="hidden lg:inline h-8 w-auto"
+                      src="/name.svg"
+                      alt="Workflow"
+                    />
+                  </Link>
+                </div>
+                <div className="hidden sm:block sm:ml-6">
+                  <div className="flex space-x-4">
+                    {navigation.map((item) => (
+                      <NavLink
+                        exact
+                        key={item.name}
+                        to={item.href}
+                        className={`px-3 py-2 rounded-md text-sm font-medium
+                        ${
+                          location.pathname !== item.href
+                            ? "text-gray-300 hover:bg-gray-700 hover:text-white"
+                            : ""
+                        }
+                        `}
+                        activeClassName={"bg-gray-900 text-white"}
+                        aria-current={item.current ? "page" : undefined}
+                      >
+                        {item.name}
+                      </NavLink>
+                    ))}
+                  </div>
+                </div>
+              </div>
+              <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
+                <button className="bg-gray-800 p-1 rounded-full text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white">
+                  <span className="sr-only">View notifications</span>
+                  <HiOutlineBell className="h-6 w-6" aria-hidden="true" />
+                </button>
 
-export default React.memo(Header);
+                {/* Profile dropdown */}
+                <Menu as="div" className="ml-3 relative">
+                  {({ open }) => (
+                    <>
+                      <div>
+                        <Menu.Button className="bg-gray-800 flex text-sm rounded-full focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white">
+                          <span className="sr-only">Open user menu</span>
+                          <img
+                            className="h-8 w-8 rounded-full"
+                            src={`https://gravatar.com/avatar/${
+                              isAuthenticated
+                                ? md5(user.email.toLocaleLowerCase())
+                                : "0"
+                            }?s=256`}
+                            alt=""
+                          />
+                        </Menu.Button>
+                      </div>
+                      <Transition
+                        show={open}
+                        as={Fragment}
+                        enter="transition ease-out duration-100"
+                        enterFrom="transform opacity-0 scale-95"
+                        enterTo="transform opacity-100 scale-100"
+                        leave="transition ease-in duration-75"
+                        leaveFrom="transform opacity-100 scale-100"
+                        leaveTo="transform opacity-0 scale-95"
+                      >
+                        <Menu.Items
+                          static
+                          className="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none"
+                        >
+                          {isAuthenticated && (
+                            <>
+                              <Menu.Item>
+                                {({ active }) => (
+                                  <Link
+                                    to="/profile"
+                                    className={classNames(
+                                      active ? "bg-gray-100" : "",
+                                      "block px-4 py-2 text-sm text-gray-700"
+                                    )}
+                                  >
+                                    Your Profile
+                                  </Link>
+                                )}
+                              </Menu.Item>
+                              <Menu.Item>
+                                {({ active }) => (
+                                  <Link
+                                    to="/settings"
+                                    className={classNames(
+                                      active ? "bg-gray-100" : "",
+                                      "block px-4 py-2 text-sm text-gray-700"
+                                    )}
+                                  >
+                                    Settings
+                                  </Link>
+                                )}
+                              </Menu.Item>
+                              <Menu.Item>
+                                {({ active }) => (
+                                  <Link
+                                    to="#"
+                                    onClick={logout}
+                                    className={classNames(
+                                      active ? "bg-gray-100" : "",
+                                      "block px-4 py-2 text-sm text-gray-700"
+                                    )}
+                                  >
+                                    Log Out
+                                  </Link>
+                                )}
+                              </Menu.Item>
+                            </>
+                          )}
+                          {!isAuthenticated && (
+                            <Menu.Item>
+                              {({ active }) => (
+                                <Link
+                                  to="#"
+                                  onClick={loginWithRedirect}
+                                  className={classNames(
+                                    active ? "bg-gray-100" : "",
+                                    "block px-4 py-2 text-sm text-gray-700"
+                                  )}
+                                >
+                                  Log In
+                                </Link>
+                              )}
+                            </Menu.Item>
+                          )}
+                        </Menu.Items>
+                      </Transition>
+                    </>
+                  )}
+                </Menu>
+              </div>
+            </div>
+          </div>
+
+          <Disclosure.Panel className="sm:hidden">
+            <div className="px-2 pt-2 pb-3 space-y-1">
+              {navigation.map((item) => (
+                <NavLink
+                  exact
+                  key={item.name}
+                  to={item.href}
+                  className={`block px-3 py-2 rounded-md text-base font-medium
+                  ${
+                    location.pathname !== item.href
+                      ? "text-gray-300 hover:bg-gray-700 hover:text-white"
+                      : ""
+                  }
+                  `}
+                  activeClassName={"bg-gray-900 text-white"}
+                  aria-current={item.current ? "page" : undefined}
+                >
+                  {item.name}
+                </NavLink>
+              ))}
+            </div>
+          </Disclosure.Panel>
+        </>
+      )}
+    </Disclosure>
+  );
+}
