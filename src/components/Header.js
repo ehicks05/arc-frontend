04945801a158391b0b91b996441089c2c2a5270d
Eric Hicks <ehicks05@yahoo.com>
2021-07-05 21:50:56 -0400

    add logo

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,9 @@ import { Link } from "react-router-dom";
 const Header = ({ title, showSettings, setShowSettings }) => {
   return (
     <header className="flex p-4 justify-between">
-      <Link to="/">{title}</Link>
+      <Link to="/">
+        <img src="arclogo.png" alt="logo" className="h-16" />
+      </Link>
       <div role="button" onClick={() => setShowSettings(!showSettings)}>
         {showSettings ? <FaTimes /> : <FaBars />}
       </div>
