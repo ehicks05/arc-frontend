76db780c0f34b72cf417ab3e8c2d986b97999907
Eric Hicks <ehicks05@yahoo.com>
2021-07-05 21:47:26 -0400

    partial wiring of upvote/downvote. only saves to local state.

--- src/components/PostStub.js
+++ src/components/PostStub.js
@@ -1,15 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
+import { useAuth0 } from "@auth0/auth0-react";
 import { Link } from "react-router-dom";
 import TimeAgo from "timeago-react";
 
 import { VoteInput } from "./index";
 
-const userVote = {};
-const user = {};
-const upvote = () => {};
-const downvote = () => {};
-
 const PostStub = ({ post, i }) => {
+  const { user } = useAuth0();
+  const [userVote, setUserVote] = useState({});
+
+  const upvote = (userId, postId) => {
+    console.log(user);
+    console.log(`${userId}, ${postId}`);
+    setUserVote({ upvoted: !userVote.upvoted });
+  };
+  const downvote = (userId, postId) => {
+    console.log(`${userId}, ${postId}`);
+    setUserVote({ downvoted: !userVote.downvoted });
+  };
+
   const bgClass =
     i % 2 === 0 ? "bg-gray-50 dark:bg-gray-900" : "bg-white dark:bg-black";
   return (
@@ -21,8 +30,8 @@ const PostStub = ({ post, i }) => {
         <VoteInput
           upvoted={userVote.upvoted}
           downvoted={userVote.downvoted}
-          handleUpvote={() => upvote(user.id, post.id)}
-          handleDownvote={() => downvote(user.id, post.id)}
+          handleUpvote={() => upvote(user.sub, post.id)}
+          handleDownvote={() => downvote(user.sub, post.id)}
         />
         <div className="">
           <div className="text-lg">
