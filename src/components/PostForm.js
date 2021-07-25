61d51818ccad9e9f1cb6b40c23de1b623c1e7ae7
Eric Hicks <ehicks05@yahoo.com>
2021-07-25 12:26:09 -0400

    fix mutation arg structure, fix redirect, add basic formatting

--- src/components/PostForm.js
+++ src/components/PostForm.js
@@ -1,54 +1,72 @@
 import React, { useState } from "react";
-import { useQueryClient } from "react-query";
+import { useHistory } from "react-router-dom";
 import { Button } from "./index";
 import { useCreatePostMutation } from "../generated/graphql";
 
 const PostForm = () => {
+  const history = useHistory();
+
   const [title, setTitle] = useState("");
   const [link, setLink] = useState("");
   const [content, setContent] = useState("");
-  const queryClient = useQueryClient();
 
   const [createPost, { loading, error }] = useCreatePostMutation();
 
   const handleClick = async () => {
-    const data = {
+    const input = {
       title,
       link,
       content,
     };
 
-    await createPost({ variables: data });
-    queryClient.invalidateQueries("posts");
+    try {
+      const result = await createPost({ variables: { input } });
+      const newPostId = result?.data.createPost.id;
 
-    // navigate somewhere?
+      history.push(`/posts/${newPostId}`);
+    } catch (err) {}
   };
 
   if (loading) return <div>loading...</div>;
-  if (error) return <div>{error}</div>;
+  if (error) return <div>{error.message}</div>;
 
   return (
     <div className="">
-      <input
-        type="text"
-        className="border dark:border-gray-600 p-1 dark:bg-gray-600 dark:text-gray-100"
-        placeholder={"add a title"}
-        value={title}
-        onChange={(e) => setTitle(e.target.value)}
-      />
-      <input
-        type="text"
-        className="border dark:border-gray-600 p-1 dark:bg-gray-600 dark:text-gray-100"
-        placeholder={"add a link (optional)"}
-        value={link}
-        onChange={(e) => setLink(e.target.value)}
-      />
-      <textarea
-        className="border dark:border-gray-600 p-1 dark:bg-gray-600 dark:text-gray-100"
-        placeholder={"add a comment"}
-        value={content}
-        onChange={(e) => setContent(e.target.value)}
-      />
+      <div>
+        <label>Title</label>
+        <div>
+          <input
+            type="text"
+            className="border dark:border-gray-600 p-1 dark:bg-gray-600 dark:text-gray-100"
+            placeholder={"add a title"}
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+          />
+        </div>
+      </div>
+      <div>
+        <label>Link</label>
+        <div>
+          <input
+            type="text"
+            className="border dark:border-gray-600 p-1 dark:bg-gray-600 dark:text-gray-100"
+            placeholder={"add a link (optional)"}
+            value={link}
+            onChange={(e) => setLink(e.target.value)}
+          />
+        </div>
+      </div>
+      <div>
+        <label>Content</label>
+        <div>
+          <textarea
+            className="border dark:border-gray-600 p-1 dark:bg-gray-600 dark:text-gray-100"
+            placeholder={"add content"}
+            value={content}
+            onChange={(e) => setContent(e.target.value)}
+          />
+        </div>
+      </div>
       <Button
         className="block"
         disabled={!title || !content}
