a4d589d1a36121cb605bef5bd220d72319e48a9f
Eric Hicks <ehicks05@yahoo.com>
2021-08-01 11:31:50 -0400

    combine comments and posts and sort by createdAt

--- src/components/User.js
+++ src/components/User.js
@@ -22,34 +22,44 @@ const User = () => {
   }
 
   const user = data.getUser;
+  const userItems = [...user.posts, ...user.comments].sort(
+    (o1, o2) => o2.createdAt - o1.createdAt
+  );
 
   return (
     <div className="flex flex-col gap-4">
       <div>{user.username}</div>
       <div className="flex flex-col gap-4">
-        {user.posts.map((post) => (
-          <PostStub key={post.id} post={post} />
-        ))}
-      </div>
-      <div className="flex flex-col gap-4">
-        {user.comments.map((comment) => (
-          <div key={comment.id}>
-            {comment.post.link && (
-              <a href={comment.post.link}>{comment.post.title}</a>
-            )}
-            {!comment.post.link && (
-              <Link to={`/posts/${comment.post.id}`}>{comment.post.title}</Link>
-            )}{" "}
-            by{" "}
-            {comment.post.author && (
-              <Link to={`/users/${comment.post.author?.id}`}>
-                {comment.post.author?.username}
-              </Link>
-            )}
-            {!comment.post.author && "[deleted]"}
-            <Comment key={comment.id} comment={comment} />
-          </div>
-        ))}
+        {userItems.map((item) => {
+          if (item.__typename === "Post") {
+            const post = item;
+            return <PostStub key={post.id} post={post} />;
+          }
+          if (item.__typename === "Comment") {
+            const comment = item;
+            return (
+              <div key={comment.id}>
+                {comment.post.link && (
+                  <a href={comment.post.link}>{comment.post.title}</a>
+                )}
+                {!comment.post.link && (
+                  <Link to={`/posts/${comment.post.id}`}>
+                    {comment.post.title}
+                  </Link>
+                )}{" "}
+                by{" "}
+                {comment.post.author && (
+                  <Link to={`/users/${comment.post.author?.id}`}>
+                    {comment.post.author?.username}
+                  </Link>
+                )}
+                {!comment.post.author && "[deleted]"}
+                <Comment key={comment.id} comment={comment} />
+              </div>
+            );
+          }
+          return <div>unknown item type</div>;
+        })}
       </div>
     </div>
   );
