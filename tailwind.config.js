5d8a3780682739d02bae96fec016996e6e1d8f5f
Eric Hicks <ehicks05@yahoo.com>
2021-08-11 18:09:12 -0400

    add caribbean-green

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,22 @@ module.exports = {
   purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   darkMode: "media", // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "caribbean-green": {
+          50: "#f3fefa",
+          100: "#e8fdf6",
+          200: "#c5fbe8",
+          300: "#a1f8d9",
+          400: "#5bf2bd",
+          500: "#15eda1",
+          600: "#13d591",
+          700: "#10b279",
+          800: "#0d8e61",
+          900: "#0a744f",
+        },
+      },
+    },
   },
   variants: {
     extend: {},
