cbcaef27eb6f881dc121a5afad4e6a2ea56aec46
Eric Hicks <ehicks05@yahoo.com>
2023-07-19 22:06:42 -0400

    skip

--- src/useUser.ts
+++ src/useUser.ts
@@ -4,7 +4,10 @@ import { useGetUserQuery } from './generated/graphql';
 const useUser = () => {
   const { session, user } = Auth.useUser();
 
-  const { data, loading, error } = useGetUserQuery({ variables: { id: user?.id } });
+  const { data, loading, error } = useGetUserQuery({
+    variables: { id: user?.id },
+    skip: !user,
+  });
   const username = data?.getUser?.username;
 
   return { session, user, username, loading, error };
