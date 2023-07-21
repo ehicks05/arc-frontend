31ad4fe0627b40b558e73d070655183e5309271f
Eric Hicks <ehicks05@yahoo.com>
2023-07-21 14:19:04 -0400

    extract Dialog and UsernameForm

--- src/components/AuthDialog.tsx
+++ src/components/AuthDialog.tsx
@@ -1,5 +1,5 @@
-import { Dialog } from '@headlessui/react';
-import React, { useState } from 'react';
+import { Dialog as HUIDialog } from '@headlessui/react';
+import React, { ReactNode, useState } from 'react';
 import { Auth } from '@supabase/auth-ui-react';
 import { ThemeSupa } from '@supabase/auth-ui-shared';
 import { SupabaseClient } from '@supabase/supabase-js';
@@ -8,6 +8,32 @@ import useUser from '../useUser';
 import { supabase } from '../supabase';
 import { useSetUsernameMutation } from '../generated/graphql';
 import Button from './Button';
+
+interface DialogProps {
+  isOpen: boolean;
+  onClose: () => void;
+  children: ReactNode | ReactNode[];
+}
+
+const Dialog = ({ isOpen, onClose, children }: DialogProps) => (
+  <HUIDialog
+    open={isOpen}
+    onClose={onClose}
+    className="fixed z-10 inset-0 overflow-y-auto"
+  >
+    <div className="flex items-center justify-center min-h-screen">
+      <HUIDialog.Overlay className="fixed inset-0 bg-black opacity-30" />
+
+      <div className="z-20 bg-neutral-800 rounded max-w-sm mx-auto">
+        <div className="py-8 px-4 sm:px-6 lg:px-8">{children}</div>
+
+        <div className="bg-neutral-700 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
+          <button onClick={onClose}>Close</button>
+        </div>
+      </div>
+    </div>
+  </HUIDialog>
+);
 
 const AuthDialog = ({
   isOpen,
@@ -25,49 +51,29 @@ const AuthDialog = ({
   });
 
   return (
-    <Dialog
-      open={isOpen}
-      onClose={hideModal}
-      className="fixed z-10 inset-0 overflow-y-auto"
-    >
-      <div className="flex items-center justify-center min-h-screen">
-        <Dialog.Overlay className="fixed inset-0 bg-black opacity-30" />
-
-        <div className="z-20 bg-neutral-800 rounded max-w-sm mx-auto">
-          <div className="py-8 px-4 sm:px-6 lg:px-8">
-            <Container supabaseClient={supabaseClient}>
-              <Auth
-                providers={[]}
-                theme="dark"
-                appearance={{
-                  theme: ThemeSupa,
-                }}
-                supabaseClient={supabase}
-              />
-            </Container>
-          </div>
-
-          <div className="bg-neutral-700 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
-            <button onClick={hideModal}>Close</button>
-          </div>
-        </div>
-      </div>
+    <Dialog isOpen={isOpen} onClose={hideModal}>
+      <Container supabaseClient={supabaseClient}>
+        <Auth
+          providers={[]}
+          theme="dark"
+          appearance={{
+            theme: ThemeSupa,
+          }}
+          supabaseClient={supabase}
+        />
+      </Container>
     </Dialog>
   );
 };
 
-const Container: React.FC<{ supabaseClient: SupabaseClient; children: any }> = ({
-  supabaseClient,
-  children,
-}) => {
-  const { user, username, loading } = useUser();
+const UsernameForm = () => {
   const [usernameField, setUsernameField] = useState('');
   const [setUsernameMutation, { error }] = useSetUsernameMutation();
 
   const handleSubmit = async () => {
     try {
       await setUsernameMutation({ variables: { username: usernameField } });
-      await supabaseClient.auth.refreshSession();
+      await supabase.auth.refreshSession();
     } catch (e) {
       console.log(e);
     }
@@ -77,6 +83,27 @@ const Container: React.FC<{ supabaseClient: SupabaseClient; children: any }> = (
     setUsernameField(e.target.value);
   };
 
+  return (
+    <>
+      <div>Create a username to start posting!</div>
+      <input
+        className="p-1"
+        placeholder="username"
+        value={usernameField}
+        onChange={onChange}
+      />
+      {error?.message && error.message}
+      <Button onClick={handleSubmit}>Submit</Button>
+    </>
+  );
+};
+
+const Container: React.FC<{ supabaseClient: SupabaseClient; children: any }> = ({
+  supabaseClient,
+  children,
+}) => {
+  const { user, username, loading } = useUser();
+
   if (loading) {
     return 'Loading...';
   }
@@ -85,19 +112,7 @@ const Container: React.FC<{ supabaseClient: SupabaseClient; children: any }> = (
     return (
       <div className="flex flex-col gap-4">
         <div>Welcome {username || user.email}!</div>
-        {!username && (
-          <>
-            <div>Create a username to start posting!</div>
-            <input
-              className="p-1"
-              placeholder="username"
-              value={usernameField}
-              onChange={onChange}
-            />
-            {error?.message && error.message}
-            <Button onClick={handleSubmit}>Submit</Button>
-          </>
-        )}
+        {!username && <UsernameForm />}
         <div className="h-12" />
         <Button onClick={() => supabaseClient.auth.signOut()}>Sign out</Button>
       </div>
