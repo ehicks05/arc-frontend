d0dae0bbb650efb432c39e72139de77571e87378
Eric Hicks <ehicks05@yahoo.com>
2023-07-24 00:11:05 -0400

    use headless components

--- src/components/Dialog.tsx
+++ src/components/Dialog.tsx
@@ -13,16 +13,17 @@ const Dialog = ({ isOpen, onClose, children }: DialogProps) => (
     onClose={onClose}
     className="fixed z-10 inset-0 overflow-y-auto"
   >
-    <div className="flex items-center justify-center min-h-screen">
-      <HUIDialog.Overlay className="fixed inset-0 bg-black opacity-30" />
-
-      <div className="z-20 bg-neutral-800 rounded max-w-sm mx-auto">
+    <HUIDialog.Overlay className="fixed inset-0 bg-black opacity-30" />
+    <div className="fixed inset-0 flex items-center justify-center p-4">
+      <HUIDialog.Panel className="bg-neutral-800">
         <div className="py-8 px-4 sm:px-6 lg:px-8">{children}</div>
 
-        <div className="bg-neutral-700 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
-          <button onClick={onClose}>Close</button>
+        <div className="bg-neutral-700 sm:flex sm:flex-row-reverse">
+          <button className="px-4 sm:px-6 py-3" onClick={onClose}>
+            Close
+          </button>
         </div>
-      </div>
+      </HUIDialog.Panel>
     </div>
   </HUIDialog>
 );
