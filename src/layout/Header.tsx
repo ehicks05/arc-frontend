e0909ace59620cb95892926ee88bb7667b18312e
Eric Hicks <ehicks05@yahoo.com>
2023-07-29 13:41:18 -0400

    extract MobileMenuButton

--- src/layout/Header.tsx
+++ src/layout/Header.tsx
@@ -13,6 +13,17 @@ const navigation = [
   { name: 'New', href: '/new' },
 ];
 
+const MobileMenuButton = ({ open }: { open: boolean }) => (
+  <Disclosure.Button className="inline-flex items-center justify-center p-2 rounded-md text-neutral-400 hover:text-white hover:bg-neutral-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
+    <span className="sr-only">Open main menu</span>
+    {open ? (
+      <HiOutlineX className="block h-6 w-6" aria-hidden="true" />
+    ) : (
+      <HiOutlineMenu className="block h-6 w-6" aria-hidden="true" />
+    )}
+  </Disclosure.Button>
+);
+
 export default function Header() {
   const location = useLocation();
   const { user, username } = useUser();
@@ -28,15 +39,7 @@ export default function Header() {
           <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
             <div className="relative flex items-center justify-between h-16">
               <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
-                {/* Mobile menu button */}
-                <Disclosure.Button className="inline-flex items-center justify-center p-2 rounded-md text-neutral-400 hover:text-white hover:bg-neutral-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
-                  <span className="sr-only">Open main menu</span>
-                  {open ? (
-                    <HiOutlineX className="block h-6 w-6" aria-hidden="true" />
-                  ) : (
-                    <HiOutlineMenu className="block h-6 w-6" aria-hidden="true" />
-                  )}
-                </Disclosure.Button>
+                <MobileMenuButton open={open} />
               </div>
               <div className="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
                 <div className="flex-shrink-0 flex items-center">
