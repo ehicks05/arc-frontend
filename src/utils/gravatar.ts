866e7b1865348c0c9074e377d2de56e49fc5bf55
Eric Hicks <ehicks05@yahoo.com>
2023-07-30 11:47:20 -0400

    only attempt to show gravatar if email is present

--- src/utils/gravatar.ts
+++ src/utils/gravatar.ts
@@ -1,7 +1,7 @@
 import md5 from 'md5';
 
-export const toGravatarHash = (email?: string) =>
-  email ? md5(email.trim().toLocaleLowerCase()) : '00000000000000000000000000000000';
+export const toGravatarHash = (email: string) =>
+  md5(email.trim().toLocaleLowerCase());
 
-export const toGravatarUrl = (email?: string) =>
-  `https://gravatar.com/avatar/${toGravatarHash(email)}?s=256`;
+export const toGravatarUrl = (email: string) =>
+  `https://gravatar.com/avatar/${toGravatarHash(email)}?s=256&d=retro`;
